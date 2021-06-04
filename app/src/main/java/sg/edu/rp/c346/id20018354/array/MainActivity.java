30c03437a6c5d6fcd47f67584e3c467afe8d79a6
Hui-Sin <20018354@myrp.edu.sg>
2021-06-04 10:02:24 +0800

    Exercise 1

--- app/src/main/java/sg/edu/rp/c346/id20018354/array/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20018354/array/MainActivity.java
@@ -3,12 +3,29 @@ package sg.edu.rp.c346.id20018354.array;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    String[] fruits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fruits = new String[3];
+        fruits[0]="apple";
+        fruits[1]="banana";
+        fruits[2]="cherry";
+
+        tv=findViewById(R.id.textViewItems);
+        String text = "Fruits\n";
+        text+="=====\n";
+        for(int i=0;i<fruits.length;i++){
+            text+=fruits[i]+"\n";
+        }
+     tv.setText(text);
     }
 }
